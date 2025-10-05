08e123cfb5ae64ee262038491556f28ba4e704f9
Lars Kroll <lkroll@Larss-MacBook-Pro.local>
2025-10-05 09:54:36 +0200

    init test project

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends Node2D
+@onready var base_ball_guy_sprite: AnimatedSprite2D = $BaseBallGuySprite
+@onready var hitbox: CollisionShape2D = $StaticBody2D/Hitbox
+@onready var state_chart: StateChart = $StateChart
+@onready var static_body_2d: StaticBody2D = $StaticBody2D
+
+func _ready() -> void:
+	base_ball_guy_sprite.frame_changed.connect(on_frame_changed)
+#	static_body_2d.body_entered.connect(_on_ball_hit)
+
+func start_hitting():
+	state_chart.send_event("hit")
+
+func on_frame_changed():
+	var current_frame = base_ball_guy_sprite.frame
+	# Example: Only enable hitbox on frames 2–4
+	if current_frame in [2, 3, 4]:
+		hitbox.disabled = false
+	else:
+		hitbox.disabled = true
+
+
+func _on_hitting_state_entered() -> void:
+	base_ball_guy_sprite.play()
+
+func _on_ball_hit(body):
+	print(body.name)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("hit"):
+		start_hitting()
